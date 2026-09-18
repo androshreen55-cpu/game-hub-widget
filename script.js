f18fe1165a32d50baac734d27f87c9b999323e84
androshreen55-cpu <androshreen55@gmail.com>
2026-09-18 18:04:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,107 @@
-const items=['Games','Web Apps','Settings','About'];let selected=0;let page='menu';const screen=document.getElementById('screen');function render(){if(page==='menu'){screen.innerHTML=items.map((name,i)=>'<div class="item '+(i===selected?'selected':'')+'">'+(i+1)+'. '+name+'</div>').join('')}else{screen.innerHTML='<div class="message"><b>'+page+'</b><br><br>Coming soon<br><br>Press BACK</div>'}}document.addEventListener('keydown',e=>{const k=e.key;if(k==='ArrowUp'){selected=(selected+items.length-1)%items.length;render()}else if(k==='ArrowDown'){selected=(selected+1)%items.length;render()}else if(k==='Enter'){page=items[selected];render()}else if(/^[1-4]$/.test(k)&&page==='menu'){selected=Number(k)-1;page=items[selected];render()}else if(k==='Escape'||k==='Backspace'){page='menu';render()}});render();
+var menuItems = ["GAMES", "WEB APPS", "TOOLS", "SETTINGS"];
+var selected = 0;
+var page = "main";
+
+function draw() {
+    var menu = document.getElementById("menu");
+    var html = "";
+
+    if (page == "main") {
+
+        for (var i = 0; i < menuItems.length; i++) {
+            if (i == selected) {
+                html += "<div style='background:white;color:black;padding-left:5px;'>";
+                html += "> " + menuItems[i];
+                html += "</div>";
+            } else {
+                html += "<div>> " + menuItems[i] + "</div>";
+            }
+        }
+
+    } else if (page == "games") {
+
+        var games = ["TETRIS", "SNAKE", "2048", "BACK"];
+
+        for (var i = 0; i < games.length; i++) {
+            if (i == selected) {
+                html += "<div style='background:white;color:black;padding-left:5px;'>";
+                html += "> " + games[i];
+                html += "</div>";
+            } else {
+                html += "<div>> " + games[i] + "</div>";
+            }
+        }
+
+    } else {
+
+        html = "<div style='text-align:center;margin-top:40px;'>";
+        html += "<b>COMING SOON</b><br><br>";
+        html += "This section is<br>under development.";
+        html += "</div>";
+    }
+
+    menu.innerHTML = html;
+}
+
+document.addEventListener("keydown", function(e) {
+
+    if (e.key == "ArrowDown") {
+        selected++;
+
+        if (page == "main" && selected >= menuItems.length) {
+            selected = 0;
+        }
+
+        if (page == "games" && selected >= 4) {
+            selected = 0;
+        }
+
+        draw();
+    }
+
+    if (e.key == "ArrowUp") {
+        selected--;
+
+        if (selected < 0) {
+            if (page == "main") {
+                selected = menuItems.length - 1;
+            } else {
+                selected = 3;
+            }
+        }
+
+        draw();
+    }
+
+    if (e.key == "Enter") {
+
+        if (page == "main") {
+
+            if (selected == 0) {
+                page = "games";
+                selected = 0;
+            } else {
+                page = "other";
+            }
+
+        } else if (page == "games") {
+
+            if (selected == 3) {
+                page = "main";
+                selected = 0;
+            } else {
+                page = "other";
+            }
+        }
+
+        draw();
+    }
+
+    if (e.key == "Escape" || e.key == "Backspace") {
+        page = "main";
+        selected = 0;
+        draw();
+    }
+});
+
+draw();
